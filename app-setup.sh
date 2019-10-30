8da217d0326ab2b00e79ec9240a4a3b3db03e40d
RobSka17 <robska17@gmail.com>
2019-10-30 10:41:12 +0000

    Added dependencies file

---
+++ app-setup.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+echo 'Getting base requirements...'
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+echo 'Getting additional requirements info...'
+pip3 freeze --local | grep -v ./ > requirements.txt
+echo $(ls)
+echo 'Initialising virtual environment...'
+echo $(ls)
+if ! ./env; then
+sudo mv -f venv ./env
+python3 -m venv env
+fi
+. env/bin/activate
+echo 'Installing additional requirements...'
+sudo pip3 install -r requirements.txt
+echo 'Exporting for launch...'
+export FLASK_APP=run.py FLASK_ENV=production FLASK_RUN_HOST=0.0.0.0 FLASK_RUN_PORT=5000 LC_ALL=C.UTF-8 LANG=C.UTF-8
+python -m flask run
