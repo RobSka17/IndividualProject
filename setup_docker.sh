f68fb8d473ddcedb7cfeab5b9e78e7131f8370c0
RobSka17 <55387266+RobSka17@users.noreply.github.com>
2019-11-01 11:22:38 +0000

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Initialising image...'
 sudo mkdir /home/$(whoami)/cyraz-wor
-sudo docker build -t /home/$(whoami)/cyraz-wor/cyraz-wor:latest .
+sudo docker build -t /home/$(whoami)/cyraz-wor/cyraz-wor .
 
 echo 'Running...'
 sudo docker run -d -p 5000:5000 cyraz-wor
