cf7b1afe041fdd0b92fe6218bf4415aef4bc7a7a
RobSka17 <55387266+RobSka17@users.noreply.github.com>
2019-11-01 11:38:25 +0000

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo mv /home/$(whoami)/IndividualProject /home/$(whoami)/cyraz-wor
-cd /home/$(whoami)/cyraz-wor
+sudo mv /home/$(whoami)/IndividualProject /home/$(whoami)/cyrazwor
+cd /home/$(whoami)/cyrazwor
 echo 'Initialising image...'
-sudo docker build -t /home/$(whoami)/cyraz-wor/cyraz-wor .
+sudo docker build -t /home/$(whoami)/cyrazwor/cyrazwor .
 
 echo 'Running...'
-sudo docker run -d -p 5000:5000 cyraz-wor
+sudo docker run -d -p 5000:5000 cyrazwor
