bda4b10fdc63c639adae60e38cc835b5d8906ad8
RobSka17 <55387266+RobSka17@users.noreply.github.com>
2019-11-01 11:58:57 +0000

    Update setup-basic.sh

--- setup-basic.sh
+++ setup-basic.sh
@@ -4,8 +4,11 @@ sudo apt-get update
 sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-venv
 
+echo 'Renaming directory to cyraz-wor...'
+sudo mv /home/$(whoami)/IndividualProject /home/$(whoami)/cyraz-wor
+cd /home/$(whoami)/cyraz-wor
+
 echo 'Initialising virtual environment...'
-cd IndividualProject 
 . venv/bin/activate
 
 echo 'Installing additional requirements...'
