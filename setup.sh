10d648f86c38a38c0642b9b7d29d9561803dc866
RobSka17 <55387266+RobSka17@users.noreply.github.com>
2019-11-01 10:18:04 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+echo 'Getting base requirements...'
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+
+echo 'Initialising virtual environment...'
+cd IndividualProject 
+. venv/bin/activate
+
+echo 'Installing additional requirements...'
+pip3 install -r requirements.txt
+
+echo 'Running...'
+python3 run.py
